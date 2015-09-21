6c4dc66185ed4b1a73cba3df5ae2bb7b121335bc
William Garcia <william.garciamoran@gmail.com>
2015-09-20 19:38:47 -0600

    Primera prueba de commit Fredy

---
+++ src/AppBundle/Controller/PruebaController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/prueba", name="prueba")
+     */
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('');
+    }
+}
