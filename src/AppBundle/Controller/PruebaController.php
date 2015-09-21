ed018425eb78b1ac9592b347e9ecad937d03d83a
William Garcia <william.garciamoran@gmail.com>
2015-09-20 19:44:24 -0600

    prueba fredy 2

--- src/AppBundle/Controller/PruebaController.php
+++ src/AppBundle/Controller/PruebaController.php
@@ -14,7 +14,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
+        //A ver si sale
         return $this->render('');
     }
 }
