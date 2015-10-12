8bf3dd78527e2b9d65036f59b1eda72d8d60ec6b
William Garcia <william.garciamoran@gmail.com>
2015-10-12 16:22:28 -0600

    Modulo de Gestion de Fechas Finalizado

--- src/AppBundle/Controller/FechaController.php
+++ src/AppBundle/Controller/FechaController.php
@@ -14,4 +14,32 @@ class FechaController extends  Controller{
     public function fechasAction(){
         return $this->render('AppBundle:admin/fechas:fechasPrincipal.html.twig');
     }
+
+    /**
+     * @Route("/fechamod", name="fechamod")
+     */
+    public function fechaMod(){
+        return $this->render('AppBundle:admin/fechas:fechasModulos.html.twig');
+    }
+
+    /**
+     * @Route("/fechaniv", name="fechaniv")
+     */
+    public function fechaNiv(){
+        return $this->render('AppBundle:admin/fechas:fechasNiveles.html.twig');
+    }
+
+    /**
+     * @Route("/fechageshoras", name="fechageshoras")
+     */
+    public function fechaGesHoras(){
+      return $this->render('AppBundle:admin/fechas:fechasGesHoras.html.twig');
+    }
+
+    /**
+     * @Route("/fechanotas", name="fechanotas")
+     */
+    public function fechaNotas(){
+        return $this->render('AppBundle:admin/fechas:fechasIngresoNotas.html.twig');
+    }
 }
