b4dd97ed596a7c66cc3afbf8abaf419edfcf27e7
William Garcia <william.garciamoran@gmail.com>
2015-10-13 11:42:40 -0600

    Modulo de Notas Primera parte

---
+++ src/AppBundle/Controller/NotasController.php
@@ -0,0 +1,24 @@
+<?php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
+
+class NotasController extends Controller{
+    /**
+     * @Route("/notasprincipal", name="notasprincipal")
+     */
+    public function notasPrincipal(){
+        return $this->render("AppBundle:notas:notasPrincipal.html.twig");
+    }
+
+    /**
+     * @Route("/ingresarnotas", name="ingresarnotas")
+     */
+    public function ingresarNotas(){
+        return $this->render("AppBundle:notas:igresarNotas.html.twig");
+    }
+}
