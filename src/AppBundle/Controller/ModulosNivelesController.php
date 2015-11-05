aff60722d5a4c8cd20c9032ceb95d6e4b3a89ea9
William Garcia <william.garciamoran@gmail.com>
2015-11-05 00:50:58 -0600

    Ya inserta niveles

--- src/AppBundle/Controller/ModulosNivelesController.php
+++ src/AppBundle/Controller/ModulosNivelesController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Nivel;
 use Proxies\__CG__\AppBundle\Entity\Modulo;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -93,8 +94,21 @@ class ModulosNivelesController extends Controller{
     }
 
     /**
-     * @Route
+     * @Route("/nnivel", name="nnivel")
      */
+    public function agregarNivelAction(Request $request){
+        $em= $this->getDoctrine()->getManager();
+        if($request->isMethod("POST")){
+            $niv= new Nivel();
+            $niv->setNombrenivel($request->get("nombreNivel"));
+            $em->persist($niv);
+            $em->flush();
+
+            return $this->redirectToRoute('newnivel');
+        }
+
+        return $this->render('AppBundle:admin/gmodulosniveles:formNuevoNivel.html.twig');
+    }
 
 
 }
