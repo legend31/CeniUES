cbbbf2b08afcc7494517abb9cc0bbc0a72385f44
William Garcia <william.garciamoran@gmail.com>
2015-11-26 11:14:20 -0600

    hoy no hay bolsa y seguimos en 8

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Resultadoevaluacion;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -34,5 +35,25 @@ class DefaultController extends Controller
     public function listaAlumno(){
         return $this->render('AppBundle:reportes:listadoalumnos.html.twig');
     }
+    /**
+     * @Route("/notas/{id}", name="notas")
+     */
+    /*public function notasAlumnoAction($id){
+        $em=$this->getDoctrine()->getManager();
+        $eva=$em->getRepository('AppBundle:Evaluacion')->find($id);
+        $detalle=$em->getRepository('AppBundle:Detalleevaluacion')->find(6);
+
+        $resultado=new Resultadoevaluacion();
+        $resultado->setAlumnoCarnetalumno($em->getRepository('AppBundle:Alumno')->find("AA12000"));
+        $resultado->setDetalleevaluaciondetalleevaluacion($detalle);
+        $resultado->setEvaluacionevaluacion($eva);
+        $resultado->setDescripcion("Prueba");
+        $resultado->setNota(7);
+
+        $em->persist($resultado);
+        $em->flush();
+
+        return new Response('Ingresado');
+    }*/
 
 }
